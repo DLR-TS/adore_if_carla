2c0d002d07c2d506d655afbd227abd76a8998e1c
Andrew Koerner <andrew.koerner@dlr.de>
2025-09-03 16:27:43 +0200

    normalized eclipse doc headers
    Signed-off-by: Andrew Koerner <andrew.koerner@dlr.de>

--- include/sensor_data_conversion_node.hpp
+++ include/sensor_data_conversion_node.hpp
@@ -1,15 +1,14 @@
 /********************************************************************************
- * Copyright (C) 2017-2025 German Aerospace Center (DLR).
- * Eclipse ADORe, Automated Driving Open Research https://eclipse.org/adore
+ * Copyright (c) 2025 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0.
+ * https://www.eclipse.org/legal/epl-2.0
  *
  * SPDX-License-Identifier: EPL-2.0
- *
- * Contributors:
- *    Matthias Nichting
  ********************************************************************************/
 
 #pragma once
